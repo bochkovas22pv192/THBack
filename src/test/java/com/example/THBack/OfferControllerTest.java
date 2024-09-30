e67ed8ce3b1908ac2ae87590cf4997931e527873
bochkovas22pv192 <bochkovsky2002@gmail.com>
2024-09-30 13:58:18 +0300

    test final

--- src/test/java/com/example/THBack/OfferControllerTest.java
+++ src/test/java/com/example/THBack/OfferControllerTest.java
@@ -260,8 +260,6 @@ class OfferControllerTest {
         MatcherAssert.assertThat(result, equalTo(offerMapper.offerToOfferPostAndPutDTO(new Offer(offersFromRep.getLast().getId(),
                 "ddd", "ddd", OfferState.WAITING,
                 LocalDate.parse("2024-09-24",dtf), employee, new LinkedHashSet<OfferRate>(), photoSet))));
-
-
     }
 
     @Test
@@ -306,160 +304,223 @@ class OfferControllerTest {
         photoSet.getFirst().setPhoto("iVBORw0KGgo".getBytes());
         photoSet.getFirst().setOffer(offersFromRep.getFirst());
         MatcherAssert.assertThat(result, equalTo(offerMapper.offerToOfferPostAndPutDTO(new Offer(offersFromRep.getFirst().getId(),
-                "ddd", "ddd", OfferState.WAITING,
+                "ddd", "ddd", OfferState.APPROVED,
                 LocalDate.parse("2024-09-24",dtf), employee, new LinkedHashSet<OfferRate>(), photoSet))));
-
-
     }
 
-//
-//    @Test
-//    void shouldCreateTasks() {
-//        String requestBody = "{\"id\":1,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        given().
-//                contentType("application/json").
-//                body(requestBody).
-//                when().
-//                post("/tasks/").
-//                then();
-//
-//        Task[] temp = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/tasks")
-//                .then().extract().body().as(Task[].class);
-//
-//        Task res = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/tasks/"+temp[0].getId())
-//                .then()
-//                .statusCode(200)
-//                .extract().body().as(Task.class);
-//
-//        MatcherAssert.assertThat(res, equalTo(new Task(temp[0].getId(),
-//                "Ivan@mail.ru", "Помыть полы", "Мыть полы", "Низкий", "Ivan@mail.ru", LocalDate.parse("2025-09-15", dtf),
-//                temp[0].getTimeCreate(), State.NEW, temp[0].getTimeUpdate())));
-//    }
-//
-//    @Test
-//    void shouldUpdateTasks() {
-//        List<Task> tasks = List.of(
-//                new Task("Petr@mail.ru", "Помыть полы", "Мыть полы", "Высокий", "Petr@mail.ru",
-//                        LocalDate.now(),  LocalDateTime.now(), State.NEW, LocalDateTime.now() )
-//        );
-//        taskRepository.saveAll(tasks);
-//
-//        Task[] taskInDb = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/tasks")
-//                .then().extract().body().as(Task[].class);
-//
-//        String requestBody = "{\"id\":1,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        given().
-//                contentType("application/json").
-//                body(requestBody).
-//                when().
-//                put("/tasks/" + taskInDb[0].getId()).
-//                then().
-//                statusCode(200);
-//
-//        Task res = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/tasks/"+taskInDb[0].getId())
-//                .then()
-//                .extract().body().as(Task.class);
-//
-//        Task[] temp = given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("/tasks")
-//                .then().extract().body().as(Task[].class);
-//
-//        MatcherAssert.assertThat(res, equalTo(new Task(taskInDb[0].getId(),
-//                "Ivan@mail.ru", "Помыть полы", "Мыть полы", "Низкий", "Ivan@mail.ru", LocalDate.parse("2025-09-15", dtf),
-//                taskInDb[0].getTimeCreate(), State.NEW, temp[0].getTimeUpdate())));
-//    }
-//
-//    private String sendPut(String requestBody){
-//        return given().
-//                contentType("application/json").
-//                body(requestBody).
-//                when().
-//                put("/tasks/2")
-//                .then()
-//                .statusCode(400)
-//                .extract()
-//                .asString();
-//    }
-//
-//    @Test
-//    void sendEmptyName() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2024-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Поле \"Название задачи\" обязательно для заполнения."));
-//    }
-//
-//    @Test
-//    void sendWhitespaceName () {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"  \",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2024-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Введено недопустимое значение поля \"Тема задачи\"."));
-//    }
-//
-//
-//    @Test
-//    void sendDateTooOld() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2024-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Срок исполнения задачи не должен быть меньше текущей даты."));
-//    }
-//
-//    @Test
-//    void sendEmptyDescription() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Поле \"Комментарий\" обязательно для заполнения."));
-//    }
-//
-//    @Test
-//    void sendWhitespaceDescription() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"   \",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Введено недопустимое значение поля \"Описание задачи\""));
-//    }
-//
-//    @Test
-//    void sendEmptyEmailExecutor() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = sendPut(requestBody);
-//        MatcherAssert.assertThat(mess, equalTo("Поле \"Исполнитель задачи\" обязательно для заполнения."));
-//    }
-//
-//    @Test
-//    void noTaskExceptionExecutor() {
-//        String requestBody = "{\"id\":2,\"email\":\"Ivan@mail.ru\",\"name\":\"Помыть полы\",\"description\":\"Мыть полы\",\"priority\":\"Низкий\",\"emailExecutor\":\"Ivan@mail.ru\",\"date\":\"2025-09-15\",\"timeCreate\":\"2024-09-15T01:32:46.406105\",\"state\":\"NEW\",\"timeUpdate\":\"2024-09-15T01:32:46.406105\"}";
-//
-//        String mess = given().
-//                contentType("application/json").
-//                body(requestBody).
-//                when().
-//                put("/tasks/2")
-//                .then()
-//                .statusCode(400)
-//                .extract()
-//                .asString();
-//        MatcherAssert.assertThat(mess, equalTo("Нет такой задачи."));
-//    }
-//
+    @Test
+    void shouldUpdateOfferStatus() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+        Employee employee2 = new Employee("Petr", "Petrov", "Ivanovich",
+                "Junior", "88005553530", LocalDate.now(), "Petr@mail.ru", "Petr",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d")
+        );
+
+        List<Offer> offers = List.of(
+                new Offer("Помыть окна", "Помыть окна", OfferState.WAITING,
+                        LocalDate.now(), employee, new LinkedHashSet<OfferRate>(), new LinkedHashSet<OfferPhoto>())
+        );
+        employeeRepository.save(employee);
+        employeeRepository.save(employee2);
+        offerRepository.saveAll(offers);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        String requestBody = "{\n" +
+                "    \"state\": \"APPROVED\"" +",\n" +
+                "    \"adminId\": "+ employees.getLast().getId()+"\n" +
+                "}";
+
+        OfferPostAndPutDTO result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .put("/api/v1/offer/status/" + offerRepository.findAll().getFirst().getId())
+                .then().statusCode(200)
+                .extract().body().as(OfferPostAndPutDTO.class);
+
+        List<Offer> offersFromRep = offerRepository.findAll();
+
+        MatcherAssert.assertThat(result, equalTo(offerMapper.offerToOfferPostAndPutDTO(new Offer(offerRepository.findAll().getFirst().getId(),
+                "Помыть окна", "Помыть окна", OfferState.APPROVED,
+                LocalDate.now(), employee, new LinkedHashSet<OfferRate>(), new LinkedHashSet<OfferPhoto>()))));
+    }
+
+    @Test
+    void shouldGetTitleEmptyException() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+
+        employeeRepository.save(employee);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        String requestBody = "{\n" +
+                "    \"authorId\": "+ employees.getFirst().getId() +",\n" +
+                "    \"title\": \"\",\n" +
+                "    \"description\": \"ddd\",\n" +
+                "    \"datePublished\": \"2024-09-24\",\n" +
+                "    \"images\": [\n" +
+                "        \"iVBORw0KGgo\"\n" +
+                "    ]\n" +
+                "}";
+
+        String result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/offer/")
+                .then().statusCode(400)
+                .extract().body().asString();
+
+
+        MatcherAssert.assertThat(result, equalTo("Поле \"Название\" обязательно для заполнения."));
+    }
+
+    @Test
+    void shouldGetDescriptionEmptyException() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+
+        employeeRepository.save(employee);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        String requestBody = "{\n" +
+                "    \"authorId\": "+ employees.getFirst().getId() +",\n" +
+                "    \"title\": \"ddd\",\n" +
+                "    \"description\": \"\",\n" +
+                "    \"datePublished\": \"2024-09-24\",\n" +
+                "    \"images\": [\n" +
+                "        \"iVBORw0KGgo\"\n" +
+                "    ]\n" +
+                "}";
+
+        String result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/offer/")
+                .then().statusCode(400)
+                .extract().body().asString();
+
+
+        MatcherAssert.assertThat(result, equalTo("Поле \"Описание\" обязательно для заполнения."));
+    }
+
+    @Test
+    void shouldGetImageCountException() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+
+        employeeRepository.save(employee);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        String requestBody = "{\n" +
+                "    \"authorId\": "+ employees.getFirst().getId() +",\n" +
+                "    \"title\": \"ddd\",\n" +
+                "    \"description\": \"ddd\",\n" +
+                "    \"datePublished\": \"2024-09-24\",\n" +
+                "    \"images\": [\n" +
+                "        \"iVBORw0KGgo1\",\n" +
+                "        \"iVBORw0KGgo2\",\n" +
+                "        \"iVBORw0KGgo3\",\n" +
+                "        \"iVBORw0KGgo4\",\n" +
+                "        \"iVBORw0KGgo5\",\n" +
+                "        \"iVBORw0KGgo6\",\n" +
+                "        \"iVBORw0KGgo7\",\n" +
+                "        \"iVBORw0KGgo8\",\n" +
+                "        \"iVBORw0KGgo9\",\n" +
+                "        \"iVBORw0KGgo10\",\n" +
+                "        \"iVBORw0KGgo11\",\n" +
+                "        \"iVBORw0KGgo12\"\n" +
+                "    ]\n" +
+                "}";
+
+        String result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/offer/")
+                .then().statusCode(400)
+                .extract().body().asString();
+        MatcherAssert.assertThat(result, equalTo("Вы не можете загрузить больше 10 фото."));
+    }
+
+    @Test
+    void shouldGetImageSizeException() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+
+        employeeRepository.save(employee);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        char[] data = new char[6000000];
+        Arrays.fill(data, 'a');
+        String string = new String(data);
+        String requestBody = "{\n" +
+                "    \"authorId\": "+ employees.getFirst().getId() +",\n" +
+                "    \"title\": \"ddd\",\n" +
+                "    \"description\": \"ddd\",\n" +
+                "    \"datePublished\": \"2024-09-24\",\n" +
+                "    \"images\": [\n" +
+                "        \""+ string +"\"\n" +
+                "    ]\n" +
+                "}";
+
+        String result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/offer/")
+                .then().statusCode(400)
+                .extract().body().asString();
+        MatcherAssert.assertThat(result, equalTo("Данное фото нельзя загрузить, загрузите фото меньшим размером."));
+    }
+
+    @Test
+    void shouldGetApprovingYourOwnOfferException() {
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich",
+                "Junior", "88005553535", LocalDate.now(), "Ivan@mail.ru", "Ivan",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d"));
+        Employee employee2 = new Employee("Petr", "Petrov", "Ivanovich",
+                "Junior", "88005553530", LocalDate.now(), "Petr@mail.ru", "Petr",
+                LocalDate.now(), HexFormat.of().parseHex("e04fd020ea3a6910a2d808002b30309d")
+        );
+
+        List<Offer> offers = List.of(
+                new Offer("Помыть окна", "Помыть окна", OfferState.WAITING,
+                        LocalDate.now(), employee, new LinkedHashSet<OfferRate>(), new LinkedHashSet<OfferPhoto>())
+        );
+        employeeRepository.save(employee);
+        employeeRepository.save(employee2);
+        offerRepository.saveAll(offers);
+
+        List<Employee> employees = employeeRepository.findAll();
+
+        String requestBody = "{\n" +
+                "    \"state\": \"APPROVED\"" +",\n" +
+                "    \"adminId\": "+ employees.getFirst().getId()+"\n" +
+                "}";
+
+        String result = given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .put("/api/v1/offer/status/" + offerRepository.findAll().getFirst().getId())
+                .then().statusCode(403)
+                .extract().body().asString();
+
+        List<Offer> offersFromRep = offerRepository.findAll();
+
+        MatcherAssert.assertThat(result, equalTo("You can't approve your own offer"));
+    }
 }
